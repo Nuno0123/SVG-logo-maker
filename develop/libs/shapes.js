ffd647e92194fe6153ab5a3b2e0ad16d2a0c10b1
Nestor <you@example.com>
2023-06-13 19:15:15 -0400

    removed file

--- develop/libs/shapes.js
+++ develop/libs/shapes.js
@@ -2,12 +2,13 @@ class Shape {
     constructor(shape, logo, shapeColor){
         this.textColor = ''
         this.shape = shape
-        this.logo = logo 
-        this.shapeColor = shapeColor 
+        this.logo = logo
+        this.shapeColor = shapeColor
     }
-
+    
     setTextColor(color) {
         this.textColor = color
     }
-}
-module.exports = Shape
+    }
+    
+    module.exports = Shape
